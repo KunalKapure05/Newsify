a6ca22077d4bceea70013d81b0b2c0f411827c28
KunalKapure05 <kapurekunal61@gmail.com>
2024-11-13 20:05:55 +0530

    Added rate-limiter for limiting the incoming api requests from same ip-address

---
+++ dist/config/rateLimiter.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_rate_limit_1 = require("express-rate-limit");
+const limiter = (0, express_rate_limit_1.rateLimit)({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+exports.default = limiter;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
 import fileUpload from 'express-fileupload';
-
-
+import helmet from 'helmet';
+import cors from 'cors';
+import limiter from './config/rateLimiter';
 import apiRoutes from './routes/api'
 
+app.use(helmet());
+app.use(cors());
+app.use(limiter);
 app.use(fileUpload()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
